18c1c263b1cf03b7c6fee00c9fe8715f53b5af91
no1crate <67573485+no1crate@users.noreply.github.com>
2021-03-09 21:21:00 -0300

    Reinicio

---
+++ script.js
@@ -0,0 +1,24 @@
+let valoresScript = {
+  bit: 0.01,
+  usd: 100
+}
+function submitBITS(){
+  bits = document.getElementById(bits).value;
+  if(bits<0){
+  console.log("No puedes poner un numero menor a 0!")
+  } else {
+  bits = bits.replace('.', '');
+  usd = valoresScript.bit * bits;
+  document.getElementById('resultado').innerHTML = "$" + usd + " USD";
+  }
+}
+function submitUSD(){
+  usd = document.getElementById(usd).value;
+  if(usd<0.01){
+  console.log("No puedes poner un numero menor a 0.01!")
+  } else {
+  usd = usd.replace('.', '');
+  bits = valoresScript.usd * usd;
+  document.getElementById('resultado').innerHTML = bits + " bit/s";
+  }
+}
