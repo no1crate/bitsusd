333247b92b78b970d06f3d9cb387f29b66cb0b80
no1crate <67573485+no1crate@users.noreply.github.com>
2021-03-09 23:32:58 -0300

    Actualizacion GET

--- bits.js
+++ bits.js
@@ -1,1 +1,9 @@
 let bits = getParameterByName('bits');
+if(bits != ""){
+    if(bits<1){
+        console.log("No puedes poner un numero menor a 0!")
+    } else {
+        usd = valoresScript.bit * bits;
+        document.getElementById('resultado').innerHTML = "$" + usd + " USD";
+    }
+}
